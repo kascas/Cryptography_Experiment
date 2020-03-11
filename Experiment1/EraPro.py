b5a0d71c5041cfc2c03d1f2fb8a38982074fce75
kascas <liangxp2360@163.com>
2020-03-11 18:12:51 +0800

    Erapro finish

--- Experiment1/EraPro.py
+++ Experiment1/EraPro.py
@@ -1,17 +1,29 @@
-import sys
+import os
+import time
 
 
 def EraPro(n):
-    list = [1] * ((n >> 6) + 1)
+    # to enable this program to run efficiently, divide and multiply operations below will change into bit operations
+    # for example, "i>>k" means "i//(2^k)", "i&((1<<k)-1)" means "i%(2^k)"
+    # every 128 odds(256 numbers, evens are not allowed) are divided into one group
+    list = [0] * ((n >> 8) + 1)
+    print(2)
     for i in range(3, n + 1, 2):
-        if not ((list[i >> 6] >> ((i >> 1) % 32)) & 1):
+        # "i" is in the "i//256"th group
+        # this odd's position is (i//2)%128
+        position = (i >> 1) & ((1 << 7) - 1)
+        if ((list[i >> 8] >> position) & 1) != 1:
             print(i)
             for j in range(i * i, n + 1, i):
-                if j % 2 == 1:
-                    list[j >> 6] |= (1 << ((j >> 1) % 32))
-    return list
+                if j & 1 == 1:
+                    position = (j >> 1) & ((1 << 7) - 1)
+                    # set "1" to this position
+                    list[j >> 8] |= (1 << position)
 
-print(sys.getsizeof(2**40))
-n=int(input("n= "))
+
+n = int(input("n= "))
+start = time.time()
 EraPro(n)
-
+end = time.time()
+print("time from start to end= {} s".format((end - start)))
+os.system("pause")
