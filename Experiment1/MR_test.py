6d3d0c7dae3e7ce03253b7f3d6b4c10f6d28574a
kascas <liangxp2360@163.com>
2020-03-11 16:25:08 +0800

    pro

--- Experiment1/MR_test.py
+++ Experiment1/MR_test.py
@@ -4,32 +4,25 @@ from Numtheory import FastExp
 
 
 def MR_test(p, r):
-    #p_1 is p-1, initialize variables
-    pt = p_1 = p - 1
+    # p_1 is p-1, initialize variables
+    q = p - 1
     temp, k, judge = 2, 0, 0
-    #compute k,q s.t. p-1=q*(2^k)
-    while (pt % temp) == 0:
-        temp *= 2
+    # compute k,q s.t. p-1=q*(2^k)
+    while q & 1 == 0:
+        q >>= 1
         k += 1
-    #temp=(p-1)/q which equals 2^k
-    temp = temp // 2
-    q = p_1 // temp
     print("r= {}".format(r), end="    ")
-    #compute r^q mod p
+    # compute r^q mod p
     r_q = FastExp(r, q, p)
     if (r_q == 1):
         print("True")
         return 0
     else:
-        temp = 1
-        #exp is q*(2^j)
         for j in range(k):
-            exp = temp * q
-            r_q = FastExp(r, exp, p)
+            r_q = FastExp(r, (1 << j) * q, p)
             if (r_q == p - 1):
                 print("True")
                 return 0
-            temp *= 2
         print("False")
         return 1
 
