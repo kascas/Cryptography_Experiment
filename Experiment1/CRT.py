7d746a09faf56e96ba921e9d28ff14a9c01b23a3
kascas <liangxp2360@163.com>
2020-03-11 16:11:47 +0800

    pro

--- Experiment1/CRT.py
+++ Experiment1/CRT.py
@@ -3,28 +3,26 @@ import os
 
 
 def CRT(b, m, n):
-    #initialize variables
-    mm, bm, bmp, result, temp = 1, 0, 0, 0, 0
-    #compute mm=m1*m2*...*mn
+    # initialize variables
+    mm, bm, bmp, result = 1, 0, 0, 0
+    # compute mm=m1*m2*...*mn
     for i in range(n):
         mm *= m[i]
-    #compute bm=mm/mi, bmp*bm mod mi=1
+    # compute bm=mm/mi, bmp*bm mod mi=1
     for i in range(n):
         bm = mm // m[i]
         bmp = GCD(bm, m[i])[1]
-        temp = bm * bmp * b[i]
-        result = (result + temp) % mm
-    #if result is negative, make it positive
+        result = (result + bm * bmp * b[i]) % mm
+    # if result is negative, make it positive
     if result <= 0:
-        temp = (result * (-1)) // mm + 1
-        result = temp * mm + result
+        result = ((result * (-1)) // mm + 1) * mm + result
     return result
 
 
 b = []
 m = []
 n = int(input("num of CRT is: "))
-#input values for args
+# input values for args
 for i in range(n):
     print("m_{}= ".format(i), end="")
     m.append(int(input()))
