1b1398db6d2c283827560c3037afb9062083abc1
kascas <liangxp2360@163.com>
2020-03-11 18:18:11 +0800

    Erapro add timer

--- Experiment1/EraPro.py
+++ Experiment1/EraPro.py
@@ -8,17 +8,20 @@ def EraPro(n):
     # every 128 odds(256 numbers, evens are not allowed) are divided into one group
     list = [0] * ((n >> 8) + 1)
     print(2)
+    count=1
     for i in range(3, n + 1, 2):
         # "i" is in the "i//256"th group
         # this odd's position is (i//2)%128
         position = (i >> 1) & ((1 << 7) - 1)
         if ((list[i >> 8] >> position) & 1) != 1:
             print(i)
+            count+=1
             for j in range(i * i, n + 1, i):
                 if j & 1 == 1:
                     position = (j >> 1) & ((1 << 7) - 1)
                     # set "1" to this position
                     list[j >> 8] |= (1 << position)
+    print("total= {}".format(count))
 
 
 n = int(input("n= "))
