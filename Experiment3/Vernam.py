9efe3494bea2d39e4c91ce8b6f85e3cf0821b965
kascas <liangxp2360@163.com>
2020-03-12 21:04:07 +0800

    Vernam finish

--- Experiment3/Vernam.py
+++ Experiment3/Vernam.py
@@ -5,8 +5,8 @@ def Vernam(f_in, f_out, key):
     s_read = f_in.read()
     L = []
     for i in range(len(s_read)):
-        L.append(bin(ord(s_read[i]) ^ ord(key[i % len(key)])).replace("0b", "").zfill(8))
-        print(chr(ord(s_read[i]) ^ ord(key[i % len(key)])))
+        L.append(chr(ord(s_read[i]) ^ ord(key[i % len(key)])))
+        print(chr(ord(s_read[i]) ^ ord(key[i % len(key)])),end="")
     s_write = "".join(L)
     f_out.write(s_write)
     f_in.close()
@@ -14,9 +14,9 @@ def Vernam(f_in, f_out, key):
     return
 
 
-# f_in_path = input("input path for input: ")
-# f_out_path = input("input path for output: ")
-f_in_path, f_out_path = "input1.txt", "output1.txt"
+f_in_path = input("input path for input: ")
+f_out_path = input("input path for output: ")
 f_in, f_out = open(f_in_path, mode="r"), open(f_out_path, mode="w")
 key = input("input key: ")
 Vernam(f_in, f_out, key)
+os.system("pause")
