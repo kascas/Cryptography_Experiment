8bf8c0e91a1fd31af1a4dd0b3752028fd9db2e81
kascas <liangxp2360@163.com>
2020-03-12 16:06:24 +0800

    MSC finish

---
+++ Experiment3/MSC.py
@@ -0,0 +1,27 @@
+import os
+
+
+def MSC(s, c):
+    result = []
+    for i in range(len(c)):
+        result.append(s[ord(c[i]) - ord('a')])
+    return "".join(result)
+
+
+def de_MSC(s, c):
+    result=[]
+    for i in range(len(c)):
+        for j in range(len(s)):
+            if (c[i]==s[j]):
+                result.append(chr(j+ord('a')))
+    return "".join(result)
+
+judge=int(input("please choose [1]crypt,[2]decrypt: "))
+if(judge==1):
+    s = str(input("input list: "))
+    c = str(input("input text: "))
+    print("result is: " + MSC(s, c))
+else:
+    s = str(input("input list: "))
+    c = str(input("input ciphertext: "))
+    print("result is: " + de_MSC(s, c))
