63de702ef9f6fb2815eeadefdcd9ebb2551fe759
kascas <liangxp2360@163.com>
2020-03-14 17:36:37 +0800

    GF_compute's multi finish

--- Experiment2/GF_compute.py
+++ Experiment2/GF_compute.py
@@ -5,21 +5,27 @@ def trans(n, type):
     return int(n, int(type))
 
 
-def GF_plus(a, b, n):
+def GF_plus(a, b, n=int("0b100011011", 2)):
     return a ^ b
 
 
-def GF_minus(a, b, n):
+def GF_minus(a, b, n=int("0b100011011", 2)):
     return a ^ b
 
 
-def GF_multi(a, b, n=int("0b000011011",2)):
-    i, result, temp = 0, 0, 0
-    while (a != 0):
-        if (a & 1) == 1:
-            result = result ^ b
-        b = b << 1
-        a = a >> 1
+def GF_multi(a, b, n=int("0b100011011", 2)):
+    result, temp= 0, a
+    while (b != 0):
+        if (b & 1) == 1:
+            result = result ^ temp
+        # temp = f(x) * (x ^ k)
+        temp = temp << 1
+        # if a7==1, then mod n to make deg(f(x)*(x^k))<8
+        if (temp >> 8) & 1 == 1:
+            temp = temp ^ n
+        #print(bin(temp).replace("0b","").zfill(8))
+        #do right-move to b
+        b = b >> 1
     return result
 
 
@@ -34,15 +40,17 @@ def GF_mod(a, b):
     return ()
 
 
+'''
 n_temp, type = input("input n and its type= ").split()
 n = trans(n_temp, type)
+'''
 a_temp, type = input("input a and its type= ").split()
 a = trans(a_temp, type)
 b_temp, type = input("input b and its type= ").split()
 b = trans(b_temp, type)
-print("GF_plus= " + bin(GF_plus(a, b, n)).replace("0b", ""))
-print("GF_minus= " + bin(GF_minus(a, b, n)).replace("0b", ""))
-print("GF_multi= " + bin(GF_multi(a, b, n)).replace("0b", ""))
-print("GF_mod= " + bin(GF_mod(a, b)).replace("0b", ""))
+print("GF_plus= " + bin(GF_plus(a, b)).replace("0b", ""))
+print("GF_minus= " + bin(GF_minus(a, b)).replace("0b", ""))
+print("GF_multi= " + bin(GF_multi(a, b)).replace("0b", ""))
+# print("GF_mod= " + bin(GF_mod(a, b)).replace("0b", ""))
 
 os.system("pause")
