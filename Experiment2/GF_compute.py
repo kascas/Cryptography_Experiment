23b110ddebf7be6ec21f911b639922f68e74de24
kascas <liangxp2360@163.com>
2020-03-15 10:40:53 +0800

    GF_compute fix a bug

--- Experiment2/GF_compute.py
+++ Experiment2/GF_compute.py
@@ -32,7 +32,7 @@ def GF_multi(a, b, n=int("0b100011011", 2)):
 
 def GF_div(a, b):
     quo, re = 0, 0
-    while (a > b):
+    while (a >= b):
         move = len(bin(a)) - len(bin(b))
         a = a ^ (b << move)
         quo += (1 << move)
