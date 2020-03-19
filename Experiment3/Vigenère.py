0cc1464696370c6945ad2393bc4f903fdf516593
kascas <liangxp2360@163.com>
2020-03-19 10:28:34 +0800

    fix vigenere

--- Experiment3/Vigenère.py
+++ Experiment3/Vigenère.py
@@ -9,6 +9,14 @@ def Vigenere(c, key):
         result_L.append(chr((ord(c[i]) - ord("a") + key_L[i % len(key)]) % 26 + ord("a")))
     return "".join(result_L)
 
+def de_Vigenere(c, key):
+    key_L, result_L = [], []
+    for i in range(len(key)):
+        key_L.append(ord(key[i]) - ord("a"))
+    for i in range(len(c)):
+        result_L.append(chr((ord(c[i]) - ord("a") - key_L[i % len(key)]) % 26 + ord("a")))
+    return "".join(result_L)
+
 
 if __name__ == "__main__":
     judge = int(input("please choose [1]crypt, [2]decrypt:  "))
@@ -19,5 +27,5 @@ if __name__ == "__main__":
     else:
         c=input("input text: ")
         key=input("input key: ")
-        print("result is: " + Vigenere(c, key))
+        print("result is: " + de_Vigenere(c, key))
     os.system("pause")
