629e59156758318d828d4838a3607fd29b84d04b
kascas <liangxp2360@163.com>
2020-04-09 18:30:56 +0800

    delete

--- Experiment5/AES-test.py
+++ Experiment5/AES-test.py
@@ -1,13 +1,19 @@
 from Crypto.Cipher import AES
+import time
+import os
 
-if __name__=="__main__":
+if __name__ == "__main__":
     mode = int(input("mode: "))
     length = int(input("keylen: "))
     message = int(input("text: "), 16).to_bytes(16, 'big')
     key = int(input("key: "), 16).to_bytes(length // 8, 'big')
+    start = time.clock()
     obj = AES.new(key, AES.MODE_ECB)
+    end = time.clock()
     if mode == 1:
         c = obj.encrypt(message)
     else:
         c = obj.decrypt(message)
     print(hex(int.from_bytes(c, 'big')))
+    print("AES took time: {} s".format((end - start)))
+    os.system("pause")
