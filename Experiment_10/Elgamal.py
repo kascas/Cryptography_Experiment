1a4e55bc5ca0a0f8104000a05838da40c18c1c62
kascas <liangxp2360@163.com>
2020-05-17 15:51:32 +0800

    delete math

---
+++ Experiment_10/Elgamal.py
@@ -0,0 +1,54 @@
+import os
+import random
+import hashlib
+from Numtheory import *
+
+
+def OS2IP(x_array):
+    xLen, x, base = len(x_array), 0, 1
+    for i in range(xLen):
+        x = x + x_array[xLen - 1 - i] * base
+        base = base * 256
+    return x
+
+
+def init(k):
+    p = prime(k)
+    g = random.randrange(2, p)
+    x = random.randrange(1, p - 1)
+    y = FastExp(g, x, p)
+    pu = (p, g, y)
+    pr = (p, g, x)
+    return (pu, pr)
+
+
+def sign(M, pr):
+    mHash = OS2IP(hashlib.sha1(M).digest())
+    p, g, x = pr
+    k = p - 1
+    while GCD(k, p - 1)[0] != 1:
+        k = random.randint(1, p - 1)
+    s1 = FastExp(g, k, p)
+    s2 = (mHash - x * s1) * Invert(k, p - 1) % (p - 1)
+    return (s1, s2)
+
+
+def verify(S, M, pu):
+    p, g, y = pu
+    s1, s2 = S
+    mHash = OS2IP(hashlib.sha1(M).digest())
+    v1 = FastExp(g, mHash, p)
+    v2 = FastExp(y, s1, p) * FastExp(s1, s2, p) % p
+    if v1 == v2:
+        return 1
+    else:
+        return -1
+
+
+if __name__ == "__main__":
+    pu, pr = init(1024)
+    m = input("m: ")
+    M = m.encode('utf-8')
+    S = sign(M, pr)
+    print(verify(S, M, pu))
+    os.system("pause")
