39936d4ab589adab6549876d3bc5ed8929bb3a12
kascas <liangxp2360@163.com>
2020-06-24 21:40:06 +0800

    delete math

--- SearchSecurity/client/WordStat.py
+++ SearchSecurity/client/WordStat.py
@@ -1,5 +1,6 @@
 import string
 from bloom import *
+import re
 
 
 def _stat_word(filename):
@@ -7,10 +8,19 @@ def _stat_word(filename):
     with open(filename, 'r', encoding='utf-8') as fp:
         for line in fp.readlines():
             line = line.strip('\n').lower()
-            # line = line.translate(str.maketrans(string.punctuation, ' ' * 32))
-            # line = line.translate(str.maketrans(string.digits, ' ' * 10))
-            for word in line.split():
-                wordList.append(word.strip(string.punctuation))
+            enre = re.compile(r'[\u0061-\u007a]|[\u0020-\u002f]|[\u005b-\u0060]|[\u003a-\u0040]|[\u007b-\u007e]')
+            cnre = re.compile(r'[\u4e00-\u9fa5]')
+            en = ''.join(enre.findall(line.lower()))
+            cn = ''.join(cnre.findall(line.lower()))
+            for word in en.split():
+                tmp = word.strip(string.punctuation)
+                if tmp != '':
+                    wordList.append(tmp)
+            '''        
+            for i in range(len(cn)):
+                wordList.append(cn[i])
+            '''
+    print(wordList)
     result = sorted(set(wordList), key=wordList.index)
     if len(result) < 1000:
         entries = 1000
